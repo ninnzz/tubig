40d509c94295cb276d1b8933518f0333a7e4d7d7
Seeree Stick <janthony.palad1234@gmail.com>
2019-08-26 12:24:35 +0800

    yung nibago mo po

--- routes.py
+++ routes.py
@@ -4,10 +4,9 @@ Define all routes here
 from config import Config
 from flask import Blueprint
 from flask import request, make_response, render_template
+from models import db, User, Stats
 
-<<<<<<< HEAD
-handler = Blueprint("router", __name__)
-
+handler = Blueprint('router', __name__)
 
 @handler.route("/")
 @handler.route("/home")
@@ -17,13 +16,6 @@ def home_feed():
 @handler.route("/about")
 def about_us():
 	return render_template('about_us.html')
-=======
-from models import db, User, Stats
-
-handler = Blueprint('router', __name__)
->>>>>>> 89d93566849c03c3a858032dc2e5b19aa3809523
-
-
 
 # TODO
 # Dito kayo mag aadd ng mga route
